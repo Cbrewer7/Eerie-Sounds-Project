837f23f90148bfca42dcac19d841693ba6eb4cb0
BRE15150326 <C-brewer@hotmail.co.uk>
2020-11-17 11:41:17 +0000

    Enemy to Player Tracker
    Enemy tracks and follows player within a specific range.

---
+++ Project Artemis/Assets/EnemyTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTracker : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float range = 3f;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) < range)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
