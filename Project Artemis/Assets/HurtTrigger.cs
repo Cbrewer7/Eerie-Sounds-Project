e468e9b59eeff2a44248dc03c9f93216c9bcaa9b
Owen H - Owen Home <owenhowarth@gmail.com>
2020-11-26 13:52:51 +0000

    Enemy Attack and Health
    Enemy can now attack the player and the player loses health

---
+++ Project Artemis/Assets/HurtTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int damage = 1;
+    public float resetTime = 0.25f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        GetComponent<Collider2D>().enabled = false;
+        Invoke("ResetTrigger", resetTime);
+        
+    }
+
+    private void ResetTrigger()
+    {
+        GetComponent<Collider2D>().enabled = true;
+    }
+
+
+}
