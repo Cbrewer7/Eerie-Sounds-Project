6e2429fc3a3cbd4a14ee14aac2688456c4b37d1c
BRE15150326 <C-brewer@hotmail.co.uk>
2020-11-30 13:07:33 +0000

    Tilemap pallet/Object Optiacy
    A tilemap pallet has been setup to setup the enviroment and a tree game prefab has been setup to add to the game world building.
    
    - 10 Tiles of Path and Grass
    - Tree Prefab (Changes transparency when player is behind object)

---
+++ Project Artemis/Assets/Scripts/ObjectOpacity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOpacity : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 0.7f);
+            //GetComponentInParent<Transform>().position = new Vector3(0, 0, 0.01f);
+            //GetComponent<Transform>().position = new Vector3(0, 0, 0.01f);
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            //GetComponentInParent<Transform>().position = new Vector3(0, 0, 0);
+            //GetComponent<Transform>().position = new Vector3(0, 0, 0);
+        }
+    }
+}
